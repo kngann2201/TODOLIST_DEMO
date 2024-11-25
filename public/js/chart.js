66e2e0f2f47f83e85ceb21267c28bdda24449dec
kngann2201 <2351050111ngan@ou.edu.vn>
2024-11-25 16:56:18 +0700

    up

--- public/js/chart.js
+++ public/js/chart.js
@@ -6,31 +6,47 @@ document.addEventListener('DOMContentLoaded', function () {
             console.log(name); //kiểm tra
             document.getElementById("name").innerHTML = `${name}`;
     //CHART
-    const xValues = ["Học tập", "Đi chơi", "Việc cần làm"];
-    var ht = document.querySelectorAll('#myTodo li#study').length;
-    var dc = document.querySelectorAll('#myTodo li#goout').length;
-    var td = document.querySelectorAll('#myTodo li#hasTodo').length;
-    console.log(ht);
-    const yValues = [ht, dc, td];
-    const barColors = [
-    "#b91d47",
-    "#00aba9",
-    "#2b5797"
-    ];
-    new Chart("todoChart", {
-    type: "pie",
-    data: {
-        labels: xValues,
-        datasets: [{
-        backgroundColor: barColors,
-        data: yValues
-        }]
-    },
-    options: {
-        title: {
-        display: true,
-        text: "Biểu đồ số lượng từng phân loại nhiệm vụ của bạn"
+    async function loadTasksForCount() {
+        try {
+            const response = await fetch(`http://localhost:5000/api/todo/list/${userId}`);
+            if (!response.ok) {
+              throw new Error('Không thể tải nhiệm vụ');
+            }
+            // console.log('API đã được gửi', response);  // Kiểm tra
+            const todos = await response.json();
+            const list = document.querySelectorAll('#myItem > option')
+            const xValues = list;
+            var ht = 0, dc = 0, td = 0;
+            todos.forEach(task => {
+                if (task.filter === "Học tập") ht++;
+                else if (task.filter === "Đi chơi") dc++;
+                else td++;  
+            }); 
+            const yValues = [ht, dc, td];
+            const barColors = [
+            "#b91d47",
+            "#00aba9",
+            "#2b5797"
+            ];
+            new Chart("todoChart", {
+            type: "pie",
+            data: {
+                labels: xValues,
+                datasets: [{
+                backgroundColor: barColors,
+                data: yValues
+                }]
+            },
+            options: {
+                title: {
+                display: true,
+                text: "Biểu đồ số lượng từng phân loại nhiệm vụ của bạn"
+                }
+            }
+    });
+        } catch (error) {
+            alert('Lỗi khi tải nhiệm vụ!!');
         }
     }
-    });
+    loadTasksForCount();
 });
